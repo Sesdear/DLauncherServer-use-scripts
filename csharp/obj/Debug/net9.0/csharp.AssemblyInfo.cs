7c119e65a67ede50b31d5bbefda8a7c910e6094e
hlnikniky <orekhovs09@gmail.com>
2026-01-17 18:05:15 +0700

    .

--- csharp/obj/Debug/net9.0/csharp.AssemblyInfo.cs
+++ csharp/obj/Debug/net9.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff47497d7cac1ce6640ffa848793bc1a2f19c147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ecb0b0b8cc508c89e4bb8b37e4e99948d2ae43")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
